f1b11d690fdf721deef8924030b8ea12577c5fb1
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-01-14 13:47:24 +0100

    empiezo ej2

---
+++ Ej2/scripts/galeriaJquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){//todo lo que debe esperar a que se cargue la pagina se mete en esta función
+    $("#insert").click(function(){
+        $("table").append("<td><img src='https://loremflickr.com/200/200/acnh?t=+ new Date'><td>");
+    });
+
+});
+    
