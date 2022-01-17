9b1c5ea1704cf24c05769c2c1186fa424ea034b6
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-01-17 18:56:49 +0100

    corregida funcoinalidad de insertar filas

--- Ej2/scripts/galeriaJquery.js
+++ Ej2/scripts/galeriaJquery.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){//todo lo que debe esperar a que se cargue la pagina se mete en esta función
+    let contador = 0;
+    
     $("#insert").click(function(){
-        $("table").append("<td><img src='https://loremflickr.com/200/200/acnh?t=+ new Date'><td>");
+        let num = event.timeStamp;
+        contador = contador+1;
+        if(contador==1){
+            $("table").append("<tr>");
+        }
+        
+        $("tr").append("<td><img src='https://loremflickr.com/200/200/acnh?+"+num+"'></td>");
+        
+        if (contador==7){    
+            $("table").append("</tr>");
+            $("table").append("<tr>");
+            $("tr").append("<td><img src='https://loremflickr.com/200/200/acnh?+"+num+"'></td>");
+            contador=1;
+        }
     });
-
 });
     
