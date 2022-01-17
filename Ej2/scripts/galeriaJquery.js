b5ebd770430ae573260b6365b54a87430e452ccd
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-01-17 19:12:29 +0100

    correccion de tds que se añadian al final de cada fila

--- Ej2/scripts/galeriaJquery.js
+++ Ej2/scripts/galeriaJquery.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){//todo lo que debe esperar a que se cargue la pagina se mete en esta función
     let contador = 0;
-    
+    let identificador=0;
     $("#insert").click(function(){
         let num = event.timeStamp;
+        
         contador = contador+1;
+        
         if(contador==1){
-            $("table").append("<tr>");
+            $("table").append("<tr id='"+identificador+"'>");
         }
         
-        $("tr").append("<td><img src='https://loremflickr.com/200/200/acnh?+"+num+"'></td>");
+        $("#"+identificador).append("<td><img src='https://loremflickr.com/200/200/acnh?+"+num+"'></td>");
         
         if (contador==7){    
+            
             $("table").append("</tr>");
-            $("table").append("<tr>");
-            $("tr").append("<td><img src='https://loremflickr.com/200/200/acnh?+"+num+"'></td>");
-            contador=1;
+            // $("table").append("<tr>");
+            identificador++;
+            $("#"+identificador).append("<td><img src='https://loremflickr.com/200/200/acnh?+"+num+"'></td>");
+            contador=0;
+            
         }
     });
 });
